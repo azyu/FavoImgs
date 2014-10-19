af3f36a3e0273d2884441bd9f9d8a4eecb978a93
Wondoo Kang <azyu@live.com>
2014-10-20 00:39:01 +0900

    remove duplicate code

--- FavoImgs/FavoImgs/Program.cs
+++ FavoImgs/FavoImgs/Program.cs
@@ -1,55 +1,11 @@
-﻿using CoreTweet;
-using FavoImgs.Data;
-using FavoImgs.Resources;
-using FavoImgs.Security;
+﻿using FavoImgs.Data;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Net;
 using System.Reflection;
-using System.Text.RegularExpressions;
-using System.Threading;
-using System.Windows.Forms;
 
 namespace FavoImgs
 {
-    enum TweetSource
-    {
-        Invalid = 0,
-        Tweets,
-        Favorites,
-        Lists,
-    };
-
     class Program
     {
-        private static void WriteException(Exception ex)
-        {
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine(" - {0}", ex.Message);
-            Console.ResetColor();
-        }
-
-        private static void Initialize()
-        {
-            try
-            {
-                if (!Data.TweetCache.IsCreated())
-                    Data.TweetCache.Create();
-            }
-            catch
-            {
-                throw;
-            }
-        }
-
-        private static string ShowTweet(CoreTweet.Status tweet)
-        {
-            return String.Format("{0} (@{1})  -- {2}\n{3}",
-                tweet.User.Name, tweet.User.ScreenName, tweet.CreatedAt.LocalDateTime, tweet.Text);
-        }
-
         private static void ShowAppInfo()
         {
             var version = Assembly.GetEntryAssembly().GetName().Version;
--- FavoImgs/FavoImgs/TwitterClient.cs
+++ FavoImgs/FavoImgs/TwitterClient.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Net;
-using System.Reflection;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Windows.Forms;
