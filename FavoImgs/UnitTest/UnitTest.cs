29d29276b363aa4a23c273ea50b374d05a9d8e39
Wondoo Kang <azyu@live.com>
2015-02-20 13:30:56 +0900

    Add TestMethods

--- FavoImgs/UnitTest/UnitTest.cs
+++ FavoImgs/UnitTest/UnitTest.cs
@@ -13,5 +13,45 @@ namespace UnitTest
 
             Assert.AreEqual(".jpg", actual);
         }
+
+        [TestMethod]
+        public void PathHelper_FavoritesTest()
+        {
+            FavoImgs.Options options = new FavoImgs.Options();
+
+            options.DownloadPath = @"D:\Download";
+            options.TweetSource = FavoImgs.TweetSource.Favorites;
+            options.ScreenName = "happy_naru";
+            string actual = FavoImgs.PathHelper.GetSubDirectoryName(options);
+
+            Assert.AreEqual(@"D:\Download\favorites\happy_naru", actual);
+        }
+
+        [TestMethod]
+        public void PathHelper_ListsTest()
+        {
+            FavoImgs.Options options = new FavoImgs.Options();
+
+            options.DownloadPath = @"D:\Download";
+            options.TweetSource = FavoImgs.TweetSource.Lists;
+            options.ScreenName = "iiotoko";
+            options.Slug = "yaranaika";
+            string actual = FavoImgs.PathHelper.GetSubDirectoryName(options);
+
+            Assert.AreEqual(@"D:\Download\lists\iiotoko\yaranaika", actual);
+        }
+
+        [TestMethod]
+        public void PathHelper_TweetsTest()
+        {
+            FavoImgs.Options options = new FavoImgs.Options();
+
+            options.DownloadPath = @"D:\Download";
+            options.TweetSource = FavoImgs.TweetSource.Tweets;
+            options.ScreenName = "nozoeri";
+            string actual = FavoImgs.PathHelper.GetSubDirectoryName(options);
+
+            Assert.AreEqual(@"D:\Download\tweets\nozoeri", actual);
+        }
     }
 }
