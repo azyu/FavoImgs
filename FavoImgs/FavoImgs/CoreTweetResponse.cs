ddccafe2205b3368327e3c759d0005a6fb862767
Wondoo Kang <azyu@live.com>
2015-06-09 00:53:28 +0900

    CoreTweetResponse 클래스 추가 - SearchResult와 ListedResponse<Status>에서 공통적으로 사용할 데이터를 저장하는 클래스

---
+++ FavoImgs/FavoImgs/CoreTweetResponse.cs
@@ -0,0 +1,13 @@
+﻿using CoreTweet;
+using System.Collections.Generic;
+
+namespace FavoImgs
+{
+    class CoreTweetResponse
+    {
+        public List<Status> Tweets { get; set; }
+
+        public string Json { get; set; }
+        public RateLimit RateLimit { get; set; }
+    }
+}
